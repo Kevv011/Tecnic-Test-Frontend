5b8b33ce039d1b57391d6b8074cca3fc2abad466
KevArevalo026 <kevinarepalacios123@gmail.com>
2025-05-13 22:44:42 -0600

    fix: Colocando Base URL correcta segun documentacion Rick & Morty API

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://rickandmortyapi.com/';
+const API_BASE_URL = 'https://rickandmortyapi.com/api';
 
 // Instancia de Axios para HTTP (apiClient)
 export const apiClient = axios.create({
