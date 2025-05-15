10e3e7b4bd26f9029cbe46e1a26ef475449bbce7
KevArevalo026 <kevinarepalacios123@gmail.com>
2025-05-14 21:19:42 -0600

    syle: Instalacion y uso de Tailwind - Ejercicio Rick & Morty

--- src/components/RickMortyCharacters.vue
+++ src/components/RickMortyCharacters.vue
@@ -3,6 +3,8 @@ import { ref, onMounted } from "vue";                           // Estado reacti
 import { fetchCharacters } from "../api/characters";            // Funcion que procesa 6 personajes
 import { getEpisodeName } from "../api/firstAppearanceEpisode"; // Funcion que procesa la primera ubicacion
 
+import rickMorty from '../assets/rickMorty.png';
+
 // Definicion de estados reactivos con ref
 const characters = ref([]);
 const loading = ref(false);
@@ -25,7 +27,6 @@ async function load() {
 
     // Actualizacion para mostrar los datos (re-renderizado)
     characters.value = data;
-
   } catch (err) {
     error.value = err;
   } finally {
@@ -38,8 +39,10 @@ onMounted(load);
 </script>
 
 <template>
-  <section>
-    <h1>Personajes Rick & Morty</h1>
+  <section class="text-center">
+    <div class="flex justify-center mb-4">
+      <img :src="rickMorty" alt="Rick&Morty" class="max-w-full h-auto w-70 sm:w-80 md:w-90" />
+    </div>
 
     <div v-if="loading">Cargando personajes…</div>
 
@@ -48,15 +51,46 @@ onMounted(load);
       <pre>{{ error.message }}</pre>
     </div>
 
-    <ul v-else>
-      <li v-for="char in characters" :key="char.id">
-        <img :src="char.image" :alt="`imagen de ${char.name}`" >
-        <h2>{{ char.name }}</h2>
-        <p><strong>Estado:</strong> {{ char.status }}</p>
-        <p><strong>Ultima Ubicación:</strong> {{ char.location.name }}</p>
+    <ul class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 list-none mt-8" v-else>
+      <li class="border border-gray-200 shadow-lg rounded-2xl p-6 hover:scale-105 transition-transform duration-200" v-for="char in characters" :key="char.id">
+        <div class="pb-4 flex flex-col items-center">
+
+          <!-- Imagen -->
+          <img class="rounded-xl w-32 h-32 object-cover mb-3 border-4 border-black shadow" :src="char.image" :alt="`imagen de ${char.name}`" />
+
+          <!-- Nombre -->
+          <h2 class="text-2xl font-extrabold">{{ char.name }}</h2>
+        </div>
+
+        <hr class="my-3 border-gray-300" />
+
+        <p class="mb-1">
+
+          <!-- Estado -->
+          <span class="font-semibold">Estado: </span>
+          <span
+            :class="{
+              'text-green-600': char.status === 'Alive',
+              'text-red-600': char.status === 'Dead',
+              'text-gray-500':
+                char.status !== 'Alive' && char.status !== 'Dead',
+            }"
+          >
+            {{ char.status || "Desconocido" }}
+          </span>
+        </p>
+
+        <p class="mb-1">
+
+          <!-- Ultima Ubicacion -->
+          <span class="font-semibold font-extrabold">Última Ubicación:</span>
+          {{ char.location?.name || "Desconocida" }}
+        </p>
         <p>
-          <strong>Visto por primera vez:</strong>
-          {{ char.firstAppearanceName }}
+
+          <!-- Donde se vio por primera vez -->
+          <span class="font-semibold font-extrabold">Visto por primera vez:</span>
+          {{ char.firstAppearanceName || "Desconocido" }}
         </p>
       </li>
     </ul>
